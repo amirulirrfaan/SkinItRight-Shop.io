2aa023de850366140000baced45887e12f6ce059
amirulirrfaan <amirulirrfaan@gmail.com>
2021-08-19 20:42:18 +0800

    Added new files

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+// script for responsive navbar
+
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+	if(MenuItems.style.maxHeight == "0px")
+	{
+		MenuItems.style.maxHeight = "200px";
+	}
+	else
+	{
+		MenuItems.style.maxHeight = "0px";
+	}
+}
+
+
+// script for go top button
+
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 40){
+			$('#gotopbtn').fadeIn();
+		} else{
+			$('#gotopbtn').fadeOut();
+		}
+
+	});
+
+	$("#gotopbtn").click(function(){
+		$('html,body').animate({scrollTop : 0},800);
+	});
+});
+
+
+// script for animation
+
+AOS.init({
+	offset: 400,
+	duration: 800
+});
+
+
+// scipt for product slide
+
+function imgSlider(anything){
+	document.querySelector('.zarzou').src = anything;
+}
+
+function changeBgColor(color){
+	const sec = document.querySelector('.sec');
+	sec.style.background = color;
+}
+
+// scipt for product gallery
